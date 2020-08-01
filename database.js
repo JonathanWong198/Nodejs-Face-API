4feea20109b255fd3f20ee79846133272c998e1c
Jonathan Wong <JonathanWong198@gmail.com>
2020-08-01 13:54:09 -0400

    adding ssl process to database config

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const Pool = require("pg").Pool;
+const pg = require('pg');
 
 
 // Configuration 
@@ -8,7 +9,8 @@ const pool = new Pool({
     // host : "localhost",
     // port : 5432, 
     // database : "selfiedata"
-    connectionString : process.env.DATABASE_URL
+    connectionString : process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL ? true : false
 });
 
 module.exports = pool; 
