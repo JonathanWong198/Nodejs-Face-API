094f6c1c9170e79f09b1c38b591cc288f3e3dce4
Jonathan Wong <JonathanWong198@gmail.com>
2020-08-01 13:27:29 -0400

    Connecting to URI

--- database.js
+++ database.js
@@ -3,11 +3,12 @@ const Pool = require("pg").Pool;
 
 // Configuration 
 const pool = new Pool({
-    user : "postgres", 
-    password : "LetMeIn",
-    host : "localhost",
-    port : 5432, 
-    database : "selfiedata"
+    // user : "postgres", 
+    // password : "LetMeIn",
+    // host : "localhost",
+    // port : 5432, 
+    // database : "selfiedata"
+    connectionString : process.env.DATABASE_URL
 });
 
 module.exports = pool; 
